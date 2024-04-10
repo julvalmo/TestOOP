fcbfa278e9932d5ce41a2f3e81606bd0b3cd1447
Angelo Valderrama Molina <juliothvalmo@outlook.com>
2024-04-10 16:52:26 -0500

    Testing exceptions for transactions from Main

--- TestOOP/Program.cs
+++ TestOOP/Program.cs
@@ -2,13 +2,68 @@
 using MiscClasses;
 using System.Security.Principal;
 
-
-BankAccount oAccount1 = new BankAccount("Jules", 10000);
+BankAccount oAccount1;
+try
+{
+    oAccount1 = new BankAccount("Jules", 10000);
+} // catch if the account creation is invalid by the exception generated in the class
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
 Console.WriteLine($"New Account was created for {oAccount1.Owner} with a balance of USD: {oAccount1.Balance}");
 
-oAccount1.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+// check for a valid overdraw
+try
+{
+    oAccount1.MakeWithdrawal(300, DateTime.Now, "Attempt to overdraw, not enought funds");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
 Console.WriteLine(oAccount1.Balance);
-oAccount1.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+
+// check for invalid overdraw
+try
+{
+    oAccount1.MakeWithdrawal(56500, DateTime.Now, "Attempt to overdraw, not enought funds");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine(oAccount1.Balance);
+
+// check valid deposit transaction
+try
+{
+    oAccount1.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to deposit a negative value");
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine(oAccount1.Balance);
+
+// check invalid deposit transaction
+try
+{
+    oAccount1.MakeDeposit(-10, DateTime.Now, "Friend paid me back? WRONG");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to deposit a negative value");
+    Console.WriteLine(e.ToString());
+}
+
 Console.WriteLine(oAccount1.Balance);
 
 // List all the transactions from the account
