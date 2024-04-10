2cf88b57a6560e1b84e5328cf1e9e71645935211
julio <julio@TECHSOFT401>
2024-04-10 16:24:18 -0500

    Add project files.

---
+++ TestOOP/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiscClasses;
+
+
+BankAccount oAccount1 = new BankAccount("Jules", 10000);
+Console.WriteLine($"New Account was created for {oAccount1.Owner} with a balance of USD: {oAccount1.Balance}");
